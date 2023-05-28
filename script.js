e1c0c0b635e42966378b876ecfff4b77a601a60b
tyler2089 <tyguy208@gmail.com>
2023-05-28 12:25:56 -0700

    Formatted each section to their own page + extra styling

--- script.js
+++ script.js
@@ -1,22 +1,32 @@
-let projects = document.getElementsByClassName('projects');
+let projects = document.getElementsByClassName('project-img');
+let projectContainer = document.getElementsByClassName('projects');
 let projectTags = document.getElementsByClassName('project-tag');
 let aboutMeText = document.getElementById('about-me-text');
 let tylerPic = document.getElementById('tyler-pic');
 let navBarLinks = document.getElementsByClassName('hover-underline-animation');
+let aboutMeBoxes = document.getElementsByClassName('text');
+
+
 // Mobile Friendly
 let width = window.outerWidth;
 let height = window.outerHeight;
 if (width < 1000)
 {
-    for (let i = 0; i < projects.length; i++)
+    for (let i = 0; i < projectContainer.length; i++)
     {
-        projects[i].style.width = '80%'
-        projects[i].style.height = '50%';
+        projectContainer[i].style.width = '80%'
+        projectContainer[i].style.height = '50%';
     }
 
     for (let i = 0; i < navBarLinks.length; i++)
     {
         navBarLinks[i].style.margin = '0% 3%';
+    }
+
+
+    for (let i = 0; i < aboutMeBoxes.length; i++)
+    {
+        aboutMeBoxes[i].style.width = '80%';
     }
     document.getElementById('main-page').className = 'main-page-mobile';
     tylerPic.style.visibility = 'hidden';
@@ -26,21 +36,16 @@ if (width < 1000)
 
 
 
-// Project Animations
+// About Me Animation
+for (let i = 0; i < aboutMeBoxes.length; i++)
+{
+    aboutMeBoxes[i].addEventListener('mouseover', function()
+    {
+        aboutMeBoxes[i].style.transform = 'translateY(4%) scale(1.1)';
+    });
 
-for (let i = 0; i < projects.length; i++)
-{
-    projects[i].addEventListener('mouseover', function() 
-    {projectTags[i].style.transform = 'scale(1.1)';
-     projectTags[i].style.transform = 'translate(-35%, 40%)'});
-    projects[i].addEventListener('mouseout', function()
-    {projectTags[i].style.transform = 'scale(1.0)'});
-}
-function untransform(projectTag)
-{
-    projectTag.style.transform = 'scale(1.0)';
-}
-function transform(projectTag)
-{
-    projectTag.style.transform = 'scale(1.1)';
+    aboutMeBoxes[i].addEventListener('mouseout', function()
+    {
+        aboutMeBoxes[i].style.transform = ('scale(1.0)');
+    });
 }
